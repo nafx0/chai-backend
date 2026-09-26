0ea3def18ec11d3c63124b3357af0036903cb926
nafxs0 <nafiulnafis@gmail.com>
2026-09-26 17:30:02 +0600

    add user and video model

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,12 +1,11 @@
-const asyncHandler = (fn) => async (req, res, next) => {
-    try {
-        await fn(req, res, next);
-    } catch (error) {
-        res.status(error.code || 500).json({
-            success: false,
-            message: error.message,
-        });
-    }
+const asyncHandler = (fn) => {
+    return (req, res, next) => {
+        Promise
+        .resolve(fn(req, res, next))
+        .catch((err) =>
+            next(err)
+        );
+    };
 };
 
 export { asyncHandler };
